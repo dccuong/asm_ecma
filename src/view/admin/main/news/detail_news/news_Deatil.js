bdf15931e9d2322ad865928fb638fe56d1648c64
cuongndph18057 <cuongksv10@gmail.com>
2022-02-09 12:09:01 +0700

    update all

--- src/view/admin/main/news/detail_news/news_Deatil.js
+++ src/view/admin/main/news/detail_news/news_Deatil.js
@@ -1,11 +1,9 @@
-import data2 from "../../../../../data2";
 
-
-
-const New_Detail = {
-    print(id) {
-        const result = data2.find((post) => post.id === id);
-        return /*html*/`
+const new_Detail = {
+  print(id) {
+    return fetch('https://61e7aa5ae32cd90017acbc44.mockapi.io/post/' + id)
+      .then(response => response.json())
+      .then(data => /*html*/`
         <div  class="mt-[20px] text-center" >
         <form class="w-[100%]  ">
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -13,33 +11,36 @@ const New_Detail = {
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
              Enter titler
             </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  type="text" value="${result.title}" >
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  type="text" value="${data.title}" >
           </div>
           <div class="w-full md:w-1/2 px-3">
             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
            Enter Decs
             </label>
-            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" value="${result.desc}">
+            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" value="${data.desc}">
           </div>
           <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
           Up File
           </label>
-          <img src="${result.img}">
+          <img src="${data.images}">
           <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  type="file" }>
         </div>
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
          id
         </label>
-        <input value="${result.id}" class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  type="number">
+        <input value="${data.id}" class="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  type="number">
       </div>
         </div>
         <button typu="submit" class="appearance-none block w-[25%] mx-auto bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white">Edit_News</button>
        
       </form>
             </div>
-        `;
-    }
+        `);
+  },
+  afterRender() {
+
+  }
 }
-export default New_Detail;
+export default new_Detail;
--- src/view/main/home/item_home.js
+++ src/view/main/home/item_home.js
@@ -1,23 +1,26 @@
-import data from "../../../data";
+import { getAll } from "../../../api/posts";
+
 const items_home = {
-  print() {
+  async print() {
+    const { data } = await getAll();
     return /*html*/` 
-        <div>
-         <div class="inline-block font-bold my-3">
-           <h3>TIN TỨC HỌC TẬP</h3>
+    <div>
+     <div class="inline-block font-bold my-3">
+       <h3>TIN TỨC HỌC TẬP</h3>
+     </div>
+     <div class="grid grid-cols-3 gap-3"> 
+     ${data.map((post) => /*html*/`
+       <div class="border-[1px] border-slate-900 py-2 px-3">
+         <div>
+           <img src="${post.images}" alt="" class="mx-auto"><br>
+           <p class="text-red-600 font-bold">${post.title} </p>
+           <p>${post.desc}</p>
          </div>
-         <div class="grid grid-cols-3 gap-3"> 
-         ${data.map((post) => /*html*/`
-           <div class="border-[1px] border-slate-900 py-2 px-3">
-             <div>
-               <img src="${post.image}" alt="" class="mx-auto"><br>
-               <p class="text-red-600 font-bold">${post.title} </p>
-               <p>${post.desc}</p>
-             </div>
-           </div>
-         `).join("")}
-         </div>
-         </div> `
+       </div>
+     `).join("")}
+     </div>
+     </div> `;
+
   }
 }
 export default items_home
