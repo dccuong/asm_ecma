9aa1b83fb75962e527ec2b46cd211ac0d12d2c5b
cuongndph18057 <cuongksv10@gmail.com>
2022-02-13 19:32:11 +0700

    update all

--- src/view/header/menu.js
+++ src/view/header/menu.js
@@ -2,20 +2,15 @@ const Menu = {
   print() {
     return/*html*/`
         <ul class="flex flex-3">
-          <li><a href="" class="block py-[16px] px-[20px] font-bold hover:bg-stone-500">Home</a>
+          <li><a href="" class="block py-[16px] px-[20px] font-bold hover:bg-red-500">Home</a>
+          </li>
+          <li><a href="/chap3" class="block py-[16px] px-[20px] font-bold hover:bg-red-500">chap3_find()</a>
           </li>
         
-          <li><a href="/chap3" class="block py-[16px] px-[20px] font-bold hover:bg-stone-500">chap3_find()</a>
-          </li>
-          <li><a href="/signin" class="block py-[16px] px-[20px] font-bold hover:bg-stone-500">sign in</a>
-          </li>
-          <li><a href="/signup" class="block py-[16px] px-[20px] font-bold hover:bg-stone-500">sign up</a>
-          </li>
-          <li><a href="/admin" class="block py-[16px] px-[20px] font-bold hover:bg-stone-500">admin</a>
-          </li>
-
         </ul>
+        
         `
   }
+
 }
 export default Menu;
