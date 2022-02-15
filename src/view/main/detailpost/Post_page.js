e1e3ae1a565349df79f2a33a44e63ad0a9a3eef0
cuongndph18057 <cuongksv10@gmail.com>
2022-02-15 22:34:59 +0700

    new

--- src/view/main/detailpost/Post_page.js
+++ src/view/main/detailpost/Post_page.js
@@ -8,22 +8,26 @@ const Post_page = {
                 <div  class="mt-[20px] text-center flex" >
                
                 <div id="divimg" class="w-[50%]  border-[1px] border-slate-900 py-2 px-3 " >
-                <img id="img1" class="w-[100%]" src="${data.images1}"/>
-                <img id="img2" class="w-[0%]" src="${data.images2}"/>
+                <img id="img1" class="w-[454px] h-[454px]" src="${data.images1}"/>
+                <img id="img2" class="w-0 h-0" src="${data.images2}"/>
                </div>
-                <div class="w-[50%]">
-                <h1 class="font-bold ">${data.title}</h1>
+                <div class="w-[50%] ">
+                <h1 class="font-bold uppercase text-red-500  ">${data.title}</h1>
                 <p>${data.desc}</p>
+                <button >${data.price}</button>
                 </div>
                 <hr>
                 </div>
+                
                 <style>
                 #divimg:hover #img1 {
-                    width:0%
+                    width:0%;
+                    height:0%;
                 }
                 
                 #divimg:hover #img2 {
-                    width:100%
+                    width:454px;
+                    height:454px;
                 }
                 </style>
         `
