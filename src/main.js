bdd76f58a9af7ae807316c5e71dae96918f94fe1
cuongndph18057 <cuongksv10@gmail.com>
2022-02-18 20:19:52 +0700

    update page_category

--- src/main.js
+++ src/main.js
@@ -5,14 +5,16 @@ import add_News from "./view/admin/main/news/add_news/add_news";
 import Footer from "./view/footer/footer";
 import Header from "./view/header/a_header";
 
-import Chap3 from "./view/main/chap3/items_chap3";
-import Post_page from "./view/main/detailpost/Post_page";
+import Chap3 from "./view/main/khong_day/items_chap3";
 import Homepage from "./view/main/home/home";
 import dashb from "./view/admin/main/dashboard/dashb";
-import new_Detail from "./view/admin/main/news/detail_news/news_Deatil";
 import Sign_in from "./view/main/signinup/signin";
 import Sign_up from "./view/main/signinup/signup";
-import noiDung from "./view/admin/main/news/danhSachBaiViet/noiDung";
+import noiDung from "./view/admin/main/news/danhSach_prd/noiDung";
+import Prd_page from "./view/main/detailprd/pr_page";
+import prd_Detail from "./view/admin/main/news/detail_prd/Prd_Deatil";
+import by_Prd from "./view/main/detailprd/by_Prd";
+import { data } from "autoprefixer";
 
 const layout_client = () => {
     document.getElementById("foot").innerHTML = Footer.print();
@@ -24,7 +26,7 @@ const layout_ad = () => {
     document.getElementById("header").innerHTML = Header_Ad.print();
 };
 //router
-const router = new Navigo("/", { linksSelector: "a" });
+const router = new Navigo("/", { linksSelector: "a", hash: true });
 const render = async (content, id) => {
     document.getElementById("app").innerHTML = await content.print(id);
     if (content.afterRender) content.afterRender(id);
@@ -47,14 +49,27 @@ router.on({
         render(Homepage);
         layout_client();
     },
-    "/chap3": () => { render(Chap3); layout_client(); },
-    "/chap3/:id": ({ data }) => {
+    "/khong_day/:categoryId": ({ data }) => {
+
+        const { categoryId } = data;
+        console.log(categoryId)
+        render(Chap3, categoryId);
+        layout_client();
+    },
+    "/co_day/:categoryId": ({ data }) => {
+        const { categoryId } = data;
+        console.log(categoryId)
+        render(Chap3, categoryId);
+        layout_client();
+    },
+    "/tai_nghe/:id": ({ data }) => {
         const { id } = data;// tạo biến id destructuring từ data 
-        render(Post_page, id);
+        render(Prd_page, id);
         layout_client();
     },
     "/signin": () => { render(Sign_in); layout_client(); },
     "/signup": () => { render(Sign_up); layout_client(); },
+    "/buy": () => { render(by_Prd); layout_client(); },
 
     "/admin": () => {
         layout_ad();
@@ -77,7 +92,7 @@ router.on({
     "/admin/news/:id/edit": ({ data }) => {
         layout_ad();
         const { id } = data;// tạo biến id destructuring từ data 
-        render(new_Detail, id)
+        render(prd_Detail, id)
 
     },
 
